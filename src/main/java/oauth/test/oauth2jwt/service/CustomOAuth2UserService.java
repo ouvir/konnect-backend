ce7685cd67561ffc2e2951e04d006bf8ee381b31
ouvir <kilhyunii@naver.com>
2025-05-08 15:58:40 +0900

    feat: logging AOP 추가
    -OAuthLoggingAspect
    OAuth 용 logging 추가

--- src/main/java/oauth/test/oauth2jwt/service/CustomOAuth2UserService.java
+++ src/main/java/oauth/test/oauth2jwt/service/CustomOAuth2UserService.java
@@ -13,7 +13,6 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        System.out.println(oAuth2User);
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
