2e9ddc0cc610d7b90fb55eddc769b13753397f33
ouvir <kilhyunii@naver.com>
2025-05-08 11:47:39 +0900

    fix: CustomOAuth2UserService 반환 값 구현
    - UserDTO
    유저 정보를 담은 DTO 생성
    
    - CustomOAuth2User
    OAuth2User 의 구현체 구현

--- src/main/java/oauth/test/oauth2jwt/service/CustomOAuth2UserService.java
+++ src/main/java/oauth/test/oauth2jwt/service/CustomOAuth2UserService.java
@@ -1,9 +1,6 @@
 package oauth.test.oauth2jwt.service;
 
-import oauth.test.oauth2jwt.dto.GoogleResponse;
-import oauth.test.oauth2jwt.dto.KakaoResponse;
-import oauth.test.oauth2jwt.dto.NaverResponse;
-import oauth.test.oauth2jwt.dto.OAuth2Response;
+import oauth.test.oauth2jwt.dto.*;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -27,7 +24,14 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             default -> null;
         };
 
-        // 추후 작업
-        return null;
+        String username = oAuth2Response.getProvider() + "@" + oAuth2Response.getProviderId();
+
+        UserDTO userDTO = UserDTO.builder()
+                .name(oAuth2Response.getName())
+                .username(username)
+                .role("ROLE_USER")
+                .build();
+
+        return new CustomOAuth2User(userDTO);
     }
 }
