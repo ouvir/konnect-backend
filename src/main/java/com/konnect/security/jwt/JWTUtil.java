3a6fcfc1f04c5790dbe258f6dd112f92f2a1c1b4
ouvir <kilhyunii@naver.com>
2025-05-11 00:25:59 +0900

    feat: createJWT 메서드 기본 만료시간용 추가
    기간을 3시간으로 잡은 기본 JWT 만드는 메서드 추가

--- src/main/java/com/konnect/security/jwt/JWTUtil.java
+++ src/main/java/com/konnect/security/jwt/JWTUtil.java
@@ -11,32 +11,27 @@ import java.util.Date;
 
 @Component
 public class JWTUtil {
-
+    // 3시간
+    private final long DEFAULT_EXPIRATION_TIME_MS = 60 * 60 * 3 * 1000L;
     private SecretKey secretKey;
 
     public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
-
-
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
     public Long getUserId(String token) {
-
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("userId", Long.class);
     }
 
     public String getRole(String token) {
-
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
     }
 
     public Boolean isExpired(String token) {
-
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
     }
 
     public String createJwt(Long userId, String role, Long expiredMs) {
-
         return Jwts.builder()
                 .claim("userId", userId)
                 .claim("role", role)
@@ -44,5 +39,10 @@ public class JWTUtil {
                 .expiration(new Date(System.currentTimeMillis() + expiredMs))
                 .signWith(secretKey)
                 .compact();
+    }
+
+    // 3시간 JWT 생성
+    public String createJwt(Long userId, String role) {
+        return createJwt(userId, role, DEFAULT_EXPIRATION_TIME_MS);
     }
 }
