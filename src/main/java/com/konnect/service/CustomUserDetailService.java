554cbdb29b3c79a7ece256df6d16a3b138f592fb
ouvir <kilhyunii@naver.com>
2025-05-11 20:30:45 +0900

    feat: 소셜 로그인 이메일로 일반 로그인 접속 방지 코드 추가

--- src/main/java/com/konnect/service/CustomUserDetailService.java
+++ src/main/java/com/konnect/service/CustomUserDetailService.java
@@ -25,6 +25,10 @@ public class CustomUserDetailService implements UserDetailsService {
             throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + email);
         }
 
+        if (user.getOauthCode() != null) {
+            throw new UsernameNotFoundException("이메일이 소셜로그인 계정으로 사용되고 있습니다.\n소셜 로그인으로 접속해주세요." );
+        }
+
         //UserDetails에 담아 return하면 AutneticationManager가 검증
         return new CustomUserPrincipal(UserMapper.toDTO(user));
     }
