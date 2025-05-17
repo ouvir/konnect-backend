a1c27c4109365f64ce55aa88592772d91d0e1b5e
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-05-17 11:56:01 +0900

    #4: DiaryController 객체 생성

---
+++ src/main/java/com/konnect/controller/DiaryController.java
@@ -0,0 +1,11 @@
+package com.konnect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/diary")
+public class DiaryController {
+
+    
+}
