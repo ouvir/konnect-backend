bde537e56410537191bc9463062182d6beb8afd0
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-05-17 20:31:43 +0900

    #4: 다이어리 생성 DTO 객체 추가
    - 다이어리 생성시 사용되는 DTO 객체를 추가하였습니다.

---
+++ src/main/java/com/konnect/dto/CreateDiaryRequestDTO.java
@@ -0,0 +1,30 @@
+package com.konnect.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CreateDiaryRequestDTO {
+    private String title;
+
+    private String content;
+
+    // TODO: 지역 코드는 변경될 예정
+    private Long areaCode;
+
+    private List<String> tags;
+
+    private Long writerId;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private List<SelectedAttractionDTO> attractions;
+}
