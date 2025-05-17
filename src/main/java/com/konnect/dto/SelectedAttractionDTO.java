ebc40b8de132b36abc8461f839c58b1e0b1a138b
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-05-17 20:31:11 +0900

    #4: 여행 루트 DTO 생성
    - 다이어리 생성시 여행 루트 기록에 필요한 여행지 id, idx를 받는 DTO 객체를 추가하였습니다.

---
+++ src/main/java/com/konnect/dto/SelectedAttractionDTO.java
@@ -0,0 +1,10 @@
+package com.konnect.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class SelectedAttractionDTO {
+    private Long attractionId;
+    private Integer idx;
+}
