9fb692ac40e90d67ed31814330c57e8eb91524be
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-05-19 03:05:28 +0900

    #4: Diary 생성 API 추가
    - EndPoint는 /diary로 설정했습니다.

--- src/main/java/com/konnect/controller/DiaryController.java
+++ src/main/java/com/konnect/controller/DiaryController.java
@@ -1,11 +1,34 @@
 package com.konnect.controller;
 
+import com.konnect.dto.CreateDiaryRequestDTO;
+import com.konnect.dto.CreateDiaryResponseDTO;
+import com.konnect.service.DiaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/diary")
 public class DiaryController {
 
-    
+    private final DiaryService diaryService;
+
+    @PostMapping("")
+    @ResponseBody
+    public ResponseEntity<CreateDiaryResponseDTO> createDiary(
+            @RequestPart("data") CreateDiaryRequestDTO requestDTO,
+            @RequestPart(value = "images", required = false) List<MultipartFile> images
+    ) {
+        CreateDiaryResponseDTO response = diaryService.createDiary(requestDTO, images);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
