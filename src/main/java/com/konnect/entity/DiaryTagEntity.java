2cdf7d793ecbeee7846a3808a1bfe92bf1debc07
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-05-19 02:54:12 +0900

    #4: Tag 엔티티 추가
    - Tag - Diary 사이 다대다 관계를 나타내는 엔티티인 DiaryTag도 추가했습니다.

---
+++ src/main/java/com/konnect/entity/DiaryTagEntity.java
@@ -0,0 +1,23 @@
+package com.konnect.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "diarytags")
+public class DiaryTagEntity {
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diary_id", nullable = false)
+    private DiaryEntity diary;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id", nullable = false)
+    private TagEntity tag;
+}
