681e119532233993337fc155e29aade17837e707
ouvir <kilhyunii@naver.com>
2025-05-20 10:06:08 +0900

    refactor: transaction 관리 추가
    - Oauth 서비스
    - auth 서비스

--- src/main/java/com/konnect/service/UserServiceImpl.java
+++ src/main/java/com/konnect/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.konnect.entity.UserEntity;
 import com.konnect.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +13,7 @@ public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public UserDTO findById(long id) {
         UserEntity entity = userRepository.findById(id);
         UserDTO dto = UserDTO.builder()
