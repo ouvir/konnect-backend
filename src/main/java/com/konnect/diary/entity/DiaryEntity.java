c9921608a6f90b8c9e0c157711355c766e78dcc6
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-05-21 00:05:01 +0900

    feat: Diary 엔티티 생성시간 프로퍼티 추가
    - 추후에 생성순 정렬에 사용할 수 있도록 프로퍼티를 추가했습니다.

--- src/main/java/com/konnect/diary/entity/DiaryEntity.java
+++ src/main/java/com/konnect/diary/entity/DiaryEntity.java
@@ -5,13 +5,15 @@ import com.konnect.user.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "diaries")
 public class DiaryEntity {
 
@@ -21,7 +23,7 @@ public class DiaryEntity {
     private Long diaryId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -31,9 +33,6 @@ public class DiaryEntity {
     private String title;
 
     private String content;
-//TODO
-//    @Column(name = "image_total_count")
-//    private Integer imageTotalCount;
 
     @Column(name = "start_date")
     private String startDate;
@@ -44,30 +43,18 @@ public class DiaryEntity {
     @OneToMany(mappedBy = "diary", cascade = CascadeType.ALL)
     private List<DiaryTagEntity> tags = new ArrayList<>();
 
+    @Column(nullable = false)
     private String status;
+
+    @Column(
+            name = "created_at",
+            nullable = false,
+            insertable = false,
+            updatable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+    )
+    private LocalDateTime createdAt;
 
     // TODO: 여행 루트 컬럼 추가 예정
 
-    @Builder
-    public DiaryEntity(
-            UserEntity user,
-            AreaEntity area,
-            String title,
-            String content,
-//            Integer imageTotalCount,
-            String startDate,
-            String endDate,
-            List<DiaryTagEntity> tags,
-            String status
-    ) {
-        this.user = user;
-        this.area = area;
-        this.title = title;
-        this.content = content;
-//        this.imageTotalCount = imageTotalCount;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.tags = tags;
-        this.status = status;
-    }
 }
