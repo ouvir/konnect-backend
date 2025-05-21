f5f36935ffa1eef6ee67a7d5d439a744f3369624
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-05-22 00:30:29 +0900

    feat: Like Repository 인터페이스 추가 (#20)
    - JPA 인터페이스 객체로 추가했습니다.
    - 해당 레코드가 이미 존재하지 않으면, 추가하기 위해 userId, diaryId로 레코드를 조회하는 메서드를 구현했습니다.

---
+++ src/main/java/com/konnect/like/repository/LikeRepository.java
@@ -0,0 +1,8 @@
+package com.konnect.like.repository;
+
+import com.konnect.like.entity.LikeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<LikeEntity, Long> {
+    LikeEntity findByDiary_DiaryIdAndUser_UserId(Long userId, Long diaryId);
+}
