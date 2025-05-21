69f292a878bb46b262c63745b2930961e64eb9d7
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-05-22 00:29:15 +0900

    feat: Like Service 구현 객체 추가 (#20)
    - 좋아요 상태로 변경하는 함수를 구현했습니다.

--- src/main/java/com/konnect/like/controller/LikeController.java
+++ src/main/java/com/konnect/like/controller/LikeController.java
@@ -1,5 +1,34 @@
 package com.konnect.like.controller;
 
+import com.konnect.auth.dto.CustomUserPrincipal;
+import com.konnect.diary.service.exception.DiaryRuntimeException;
+import com.konnect.like.service.LikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class LikeController {
 
+    private final LikeService likeService;
+
+    @PostMapping("/diaries/{diaryId}/like")
+    public ResponseEntity likeDiary(
+            @PathVariable Long diaryId,
+            @AuthenticationPrincipal CustomUserPrincipal userDetails
+    ) {
+        try {
+            likeService.likeDiary(diaryId, userDetails.getId());
+            return ResponseEntity.ok().build();
+        } catch (DiaryRuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
 }
---
+++ src/main/java/com/konnect/like/service/LikeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.konnect.like.service;
+
+import com.konnect.diary.entity.DiaryEntity;
+import com.konnect.diary.repository.DiaryRepository;
+import com.konnect.diary.service.exception.DiaryRuntimeException;
+import com.konnect.like.entity.LikeEntity;
+import com.konnect.like.repository.LikeRepository;
+import com.konnect.user.entity.UserEntity;
+import com.konnect.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LikeServiceImpl implements LikeService {
+
+    private final DiaryRepository diaryRepository;
+    private final LikeRepository likeRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public void likeDiary(Long diaryId, Long userId) {
+        DiaryEntity diary = diaryRepository
+                .findById(diaryId)
+                .orElseThrow(() -> new DiaryRuntimeException("Diary not found with id: " + diaryId));
+
+        LikeEntity likeEntity = likeRepository.findByDiary_DiaryIdAndUser_UserId(diaryId, userId);
+
+        if (likeEntity == null) {
+            UserEntity user = userRepository.getReferenceById(userId);
+            likeEntity = new LikeEntity();
+            likeEntity.setDiary(diary);
+            likeEntity.setUser(user);
+        } else {
+            likeEntity.setDeleted(false);
+        }
+
+        likeRepository.save(likeEntity);
+    }
+}
