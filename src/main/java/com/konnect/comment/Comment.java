d90546bb7fe47abb11a4ae66028c389f7289159e
KiminSon <kimin0331@naver.com>
2025-05-22 16:18:16 +0900

    feat: ddl auto create 추가

--- src/main/java/com/konnect/comment/Comment.java
+++ src/main/java/com/konnect/comment/Comment.java
@@ -9,15 +9,26 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Comment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
     private Long commentId;
 
+    @Column(name = "user_id", nullable = false)
     private Long userId;
+
+    @Column(name = "diary_id", nullable = false)
     private Long diaryId;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
     private String content;
-    private String createdAt; // 프론트에서 문자열로 전달
-    private boolean isDeleted;
+
+    @Column(name = "created_at")
+    private String createdAt;  // 프론트에서 문자열로 전달
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
