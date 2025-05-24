c17d894fc5001d020aabfa3afee902c4903a3a33
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-05-24 16:28:27 +0900

    feat: RouteRepository 다이어리 내 모든 경로 삭제 메서드 추가

--- src/main/java/com/konnect/route/repository/RouteRepository.java
+++ src/main/java/com/konnect/route/repository/RouteRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface RouteRepository extends JpaRepository<Route, Long>, RouteRepositoryCustom {
     /** 특정 다이어리의 루트를 방문 일자·시간 오름차순으로 반환 */
     List<Route> findByDiaryDiaryIdOrderByVisitedDateAscVisitedTimeAsc(Long diaryId);
+    void deleteByDiaryDiaryId(Long diaryId);
 }
