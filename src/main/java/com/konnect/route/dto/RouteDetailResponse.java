c7850560085614dcb5fdbd4dfada3e22067d96ab
ouvir <kilhyunii@naver.com>
2025-05-24 17:10:57 +0900

    refactor: route entity 수정
    - attraction no 삭제
    - 위도, 경도, 제목 추가
    
    - 삽입 데이터 수정

--- src/main/java/com/konnect/route/dto/RouteDetailResponse.java
+++ src/main/java/com/konnect/route/dto/RouteDetailResponse.java
@@ -1,22 +1,14 @@
 package com.konnect.route.dto;
 
-import com.konnect.attraction.dto.AttractionDTO;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "루트 + 관광지 상세 응답")
+@Schema(description = "루트 상세 응답")
 public record RouteDetailResponse(
-        @Schema(description = "루트 PK", example = "10")
         Long id,
-
-        @Schema(description = "방문 일자", example = "2025-05-23")
         String visitedDate,
-
-        @Schema(description = "방문 시각", example = "09:30")
         String visitedTime,
-
-        @Schema(description = "다음 경로까지의 거리", example = "13872")
         Double distance,
-
-        @Schema(description = "관광지 상세 정보")
-        AttractionDTO attraction
+        String title,
+        Double latitude,
+        Double longitude
 ) { }
--- src/main/java/com/konnect/route/dto/RouteUpdateRequest.java
+++ src/main/java/com/konnect/route/dto/RouteUpdateRequest.java
@@ -8,13 +8,14 @@ import lombok.Builder;
 public record RouteUpdateRequest(
         @Schema(description = "방문 일자", example = "2025-05-24")
         String visitedDate,
-
         @Schema(description = "방문 시각", example = "14:10")
         String visitedTime,
-
-        @Schema(description = "변경할 관광지 PK", example = "321")
-        Integer attractionNo,
-
+        @Schema(description = "제목", example = "14:10")
+        String title,
+        @Schema(description = "위도", example = "14:10")
+        Double latitude,
+        @Schema(description = "경도", example = "14:10")
+        Double longitude,
         @Schema(description = "다음 경로까지의 거리", example = "13872")
         Double distance
 ) { }
